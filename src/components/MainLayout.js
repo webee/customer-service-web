a8d7868008f7d0baf94f6e77c527faa392dd4f5f
wang.yi <wang.yi@qinqinxiaobao.com>
2017-09-29 23:40:50 +0800

    add ui settings.

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Loader from 'react-loader';
 import { connect } from 'dva';
 import { withRouter, Route, Link, Switch, Redirect } from 'dva/router';
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu, Icon, Switch as SwitchComp, Modal, Form } from 'antd';
 import {getRootPath} from './commons/router';
 import BreadcrumbComp from './commons/BreadcrumbComp';
 import SiderComp from './commons/SiderComp';
@@ -63,6 +63,7 @@ function genSiderMenuConfigs(project_domains) {
 class MainLayout extends React.Component {
   state = {
     collapsed: false,
+    showSettingModal: false,
   };
   onCollapse = (collapsed, type) => {
     console.debug('sider collapsed: ', collapsed, type);
@@ -78,14 +79,39 @@ class MainLayout extends React.Component {
   handleMenuClick = (item) => {
     console.debug("click menu: ", item);
     const root_path = getRootPath(this.props.match.path);
-    if (item.key === 'user/logout') {
-      const { dispatch } = this.props;
-      dispatch({type: 'auth/logout', payload: `${root_path}/auth`});
+    switch (item.key) {
+      case 'user/logout':
+        const { dispatch } = this.props;
+        dispatch({type: 'auth/logout', payload: `${root_path}/auth`});
+        break;
+      case 'ui-setting':
+        this.setState({showSettingModal:true});
+        break;
     }
   };
 
   onLogoClick = () => {
     this.props.dispatch({type: 'app/fetch'});
+  };
+
+  toggleBreadcrumb = (checked) => {
+    const {dispatch} = this.props;
+    dispatch({type: 'app/setUISettings', payload: {disable_breadcrumb: !checked}});
+  };
+
+  toggleFooter = (checked) => {
+    const {dispatch} = this.props;
+    dispatch({type: 'app/setUISettings', payload: {disable_footer: !checked}});
+  };
+
+  settingModalOnOk = (e) => {
+    console.debug(e);
+    this.setState({showSettingModal:false});
+  };
+
+  settingModalOnCancel = (e) => {
+    console.debug(e);
+    this.setState({showSettingModal:false});
   };
 
   componentDidMount() {
@@ -97,11 +123,13 @@ class MainLayout extends React.Component {
   render() {
     const { match, location } = this.props;
     const root_path = getRootPath(match.path);
-    const {staff, app, project_domains } = this.props;
+    const {staff, app, project_domains, ui_settings } = this.props;
     const loaded = staff && app && project_domains;
     if (!loaded) {
       return <Loader loaded={false}/>
     }
+
+    // ui settings
     return (
       <Layout className="ant-layout-has-sider">
         <SiderComp app_name={app.title} collapsed={this.state.collapsed}
@@ -120,21 +148,28 @@ class MainLayout extends React.Component {
               {env == "dev" ? <Menu.Item key="test">
                 <Link to="/_"><Icon type="code" /><span>Test</span></Link>
               </Menu.Item> : ""}
+              <Menu.Item key="ui-setting">
+                <span><Icon type="setting" />界面设置</span>
+							</Menu.Item>
 
               <SubMenu title={<span><Icon type="user" />{staff.name}</span>}
                        key="user"
               >
-                  <Menu.Item key="user/logout">退出</Menu.Item>
-                  <ItemGroup title="常用位置">
-										<Menu.Item key="user/common/baidu">
-                      <a href="https://baidu.com" target="_blank">百度</a>
-                    </Menu.Item>
-									</ItemGroup>
+                <Menu.Item key="user/logout">退出</Menu.Item>
+                <ItemGroup title="常用位置">
+                  <Menu.Item key="user/common/baidu">
+                    <a href="https://baidu.com" target="_blank">百度</a>
+                  </Menu.Item>
+                </ItemGroup>
               </SubMenu>
             </Menu>
 					</Header>
           <Content className={styles.content}>
-            <BreadcrumbComp urlNameMap={genUrlNameMap(root_path, project_domains)}/>
+            {/* 是否显示面包屑 */}
+            {!ui_settings.disable_breadcrumb ?
+              <BreadcrumbComp urlNameMap={genUrlNameMap(root_path, project_domains)}/>
+              : ''
+            }
             <div className={styles.contentMain}>
               {/* 主内容区路由设置 */}
               <Switch>
@@ -144,10 +179,34 @@ class MainLayout extends React.Component {
                 <Route component={NotFoundPage} />
 							</Switch>
             </div>
-            <Footer className={styles.footer}>
-              webee.yw(webee.yw@gmail.com) @2017
-            </Footer>
           </Content>
+          {/* 是否显示页脚 */}
+          {!ui_settings.disable_footer?
+          <Footer className={styles.footer}>
+            webee.yw(webee.yw@gmail.com) @2017
+          </Footer>:''
+          }
+          {/* 设置modal */}
+          {this.state.showSettingModal?
+          <Modal
+            title="界面设置"
+            visible={this.state.showSettingModal}
+            onOk={this.settingModalOnOk}
+            onCancel={this.settingModalOnCancel}
+          >
+            <Form>
+              <Form.Item label="脚标" labelCol={{span:4}}>
+                <SwitchComp checkedChildren="显示" unCheckedChildren="隐藏"
+                            defaultChecked={!ui_settings.disable_footer} onChange={this.toggleFooter}/>
+              </Form.Item>
+              <Form.Item label="面包屑" labelCol={{span:4}}>
+                <SwitchComp checkedChildren="显示" unCheckedChildren="隐藏"
+                            defaultChecked={!ui_settings.disable_breadcrumb} onChange={this.toggleBreadcrumb}/>
+              </Form.Item>
+						</Form>
+          </Modal>
+            :''
+          }
         </Layout>
       </Layout>
     );
--- src/models/app.js
+++ src/models/app.js
@@ -1,4 +1,5 @@
 import * as appService from '../services/app';
+import {createProjectDomainTypeAction} from '../services/project';
 
 
 export default {
@@ -8,20 +9,38 @@ export default {
     staff: null,
     app: null,
     project_domains: null,
+    ui_settings: appService.loadUISettings(),
   },
   reducers: {
+    saveUISettings(state, {payload}) {
+      return {...state, ui_settings: {...state.ui_settings,...payload}};
+    },
     save(state, {payload}) {
       return { ...state, ...payload };
     },
   },
   effects: {
+    *setUISettings({payload}, {call, select, put}) {
+      yield put({type: 'saveUISettings', payload});
+      const ui_settings = yield select(state => state.app.ui_settings);
+      yield call(appService.saveUISettings, ui_settings);
+    },
     *fetch({ payload }, { call, put }) {  // eslint-disable-line
       const staffAppTree = yield call(appService.fetchStaffAppTree);
+      // init project state
+      for (let d of staffAppTree.project_domains) {
+        for (let t of d.types) {
+          yield put(createProjectDomainTypeAction(d.name, t.name));
+        }
+      }
       yield put({ type: 'save', payload: staffAppTree });
     },
   },
   subscriptions: {
     setup({ dispatch, history }) {  // eslint-disable-line
     },
+    block({history}) {
+      //return history.block('确定要离开?');
+    }
   },
 };
