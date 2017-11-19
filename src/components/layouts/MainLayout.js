cf29272e97d51c7059dd880b1a7d35746fd89129
wang.yi <wang.yi@qinqinxiaobao.com>
2017-11-20 01:45:00 +0800

    update components

--- src/components/layouts/MainLayout.js
+++ src/components/layouts/MainLayout.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import Loader from 'react-loader';
-import { connect } from 'dva';
-import classNames from 'classnames';
-import DocumentTitle from 'react-document-title';
-import { withRouter, Route, Link, Switch, Redirect } from 'dva/router';
-import { getRootPath, Routes } from '../../commons/router';
-import { getURLDataMapFromNavData, getMenuDataFromNavData } from '../../commons/nav';
-import NotFound from '../NotFound';
-import { Layout, Menu, Icon, Avatar, Dropdown } from 'antd';
-import Breadcrumb from './Breadcrumb';
-import SiderMenu from './SiderMenu';
-import styles from './MainLayout.less';
+import React, { Component } from "react";
+import Loader from "react-loader";
+import { connect } from "dva";
+import classNames from "classnames";
+import DocumentTitle from "react-document-title";
+import { withRouter, Route, Link, Switch, Redirect } from "dva/router";
+import { getRootPath, Routes } from "../../commons/router";
+import {
+  getURLDataMapFromNavData,
+  getMenuDataFromNavData
+} from "../../commons/nav";
+import NotFound from "../NotFound";
+import { Layout, Menu, Icon, Avatar, Dropdown, BackTop } from "antd";
+import Breadcrumb from "./Breadcrumb";
+import SiderMenu from "./SiderMenu";
+import styles from "./MainLayout.less";
 const { Header, Content, Footer } = Layout;
-
 
 @withRouter
 export default class MainLayout extends React.Component {
   state = {
-    collapsed: false,
+    collapsed: false
   };
   onCollapse = (collapsed, type) => {
-    this.setState({collapsed});
+    this.setState({ collapsed });
   };
 
   toggleCollapse = () => {
@@ -30,56 +32,96 @@ export default class MainLayout extends React.Component {
   };
 
   render() {
-    const { match, location, name, headerMenu, navData } = this.props;
-    const { onLogoClick } = this.props;
+    const { match, location, name, headerMenu, navData, onLogoClick } = this.props;
     const { bottom } = this.props;
     // 以navData为基础生成1. 导航菜单，2. 面包屑 3. routes
-    console.debug('navData: ', navData);
+    console.debug("navData: ", navData);
     const root_path = getRootPath(match.path);
-    const path = location.pathname.replace(/\/*$/,'') || '/';
+    const path = location.pathname.replace(/\/*$/, "") || "/";
     const urlDataMap = getURLDataMapFromNavData(root_path, navData);
     const menuData = getMenuDataFromNavData(root_path, navData);
-    console.debug('urlDatamap: ', urlDataMap);
-    console.debug('path: ', path);
-    const urlData = urlDataMap[path === '/' ? '' : path] || {};
+    console.debug("urlDatamap: ", urlDataMap);
+    console.debug("path: ", path);
+    const urlData = urlDataMap[path === "/" ? "" : path] || {};
 
-    const disableBreadcrumb = this.props.disableBreadcrumb || urlData.noBreadcrumb;
+    const disableBreadcrumb =
+      this.props.disableBreadcrumb || urlData.noBreadcrumb;
     const disableFooter = this.props.disableFooter || urlData.noFooter;
+    const disableHeader = this.props.disableHeader || urlData.noHeader;
+    const fixed = urlData.fixed;
 
-    const contentClassName = classNames(styles.content, { [styles.fixedContent]: urlData.fixed });
-    const contentMainClassName = classNames(styles.contentMain, { [styles.fixedContentMain]: urlData.fixed });
+    const withTrigger = disableHeader;
+    const contentClassName = classNames(styles.content, {
+      [styles.fixedContent]: fixed
+    });
+    const contentMainClassName = classNames(styles.contentMain, {
+      [styles.fixedContentMain]: fixed
+    });
     const contentStyle = {};
-    if (urlData.fixed) {
-      contentStyle['height'] = disableFooter ? 'calc(100vh - 64px)' : 'calc(100vh - 64px - 48px)';
+    if (fixed) {
+      contentStyle["height"] = disableFooter
+        ? "calc(100vh - 64px)"
+        : "calc(100vh - 64px - 48px)";
     }
 
     const layout = (
       <Layout className={`ant-layout-has-sider ${styles.layout}`}>
-        <SiderMenu root_path={root_path} path={path}
-                  name={name} collapsed={this.state.collapsed} onCollapse={this.onCollapse}
-                  onLogoClick={onLogoClick}
-                  theme="dark" mode="inline"
-                  menuData={menuData} />
-				<Layout className={styles.main}>
-          <Header className={styles.header}>
-						<Icon className={styles.trigger} type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={this.toggleCollapse}/>
-            {headerMenu}
-					</Header>
+        <SiderMenu
+          root_path={root_path}
+          path={path}
+          name={name}
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+          onLogoClick={onLogoClick}
+          withTrigger={withTrigger}
+          theme="dark"
+          mode="inline"
+          menuData={menuData}
+        />
+        <Layout className={styles.main}>
+          {disableHeader ? (
+            ""
+          ) : (
+            <Header className={styles.header}>
+              <Icon
+                className={styles.trigger}
+                type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
+                onClick={this.toggleCollapse}
+              />
+              {headerMenu}
+            </Header>
+          )}
           <Content className={contentClassName} style={contentStyle}>
-            { disableBreadcrumb ? '' :
-              <Breadcrumb root_path={root_path} path={path} urlDataMap={urlDataMap}/>
-            }
+            {disableBreadcrumb ? (
+              ""
+            ) : (
+              <Breadcrumb
+                root_path={root_path}
+                path={path}
+                urlDataMap={urlDataMap}
+              />
+            )}
             <div className={contentMainClassName}>
               <Routes path={root_path} navData={navData} NoMatch={NotFound} />
             </div>
           </Content>
-          {disableFooter ||
-          <Footer className={styles.footer}>
-            webee.yw(webee.yw@gmail.com) @2017
-          </Footer>
-          }
+          {disableFooter ? (
+            ""
+          ) : (
+            <Footer className={styles.footer}>
+              webee.yw(webee.yw@gmail.com) @2017
+            </Footer>
+          )}
           {bottom}
         </Layout>
+        {fixed ? (
+          ""
+        ) : (
+          <BackTop
+            target={() => document.getElementsByClassName(styles.main)[0]}
+            visibilityHeight={500}
+          />
+        )}
       </Layout>
     );
 
@@ -88,10 +130,6 @@ export default class MainLayout extends React.Component {
     if (urlData) {
       title = `${urlData.title} - ${name}`;
     }
-    return (
-      <DocumentTitle title={title}>
-        {layout}
-      </DocumentTitle>
-    );
+    return <DocumentTitle title={title}>{layout}</DocumentTitle>;
   }
 }
