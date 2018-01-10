51cf9bf055b468c217942969821014f534083bc0
wang.yi <wang.yi@qinqinxiaobao.com>
2018-01-10 22:59:09 +0800

    fix EllipsisText

--- src/components/EllipsisText/index.js
+++ src/components/EllipsisText/index.js
@@ -2,17 +2,23 @@ import React from "react";
 import { Tooltip } from "antd";
 import styles from "./index.less";
 
-export default class extends React.Component {
+export default class extends React.PureComponent {
+  state = {
+    scrollWidth: 0,
+    clientWidth: 0
+  };
+
   get tip() {
     const { text, tipText } = this.props;
-    const { content } = this;
-    return content && content.scrollWidth > content.clientWidth ? (tipText ? tipText : text) : undefined;
+    const { scrollWidth, clientWidth } = this.state;
+    console.debug(`EllipsisText: ${scrollWidth}/${clientWidth}`);
+    return scrollWidth > clientWidth ? (tipText ? tipText : text) : undefined;
   }
 
   render() {
-    const { text, width } = this.props;
+    const { text, width, ...props } = this.props;
     return (
-      <Tooltip title={this.tip}>
+      <Tooltip title={this.tip} {...{ mouseLeaveDelay: 0, ...props }}>
         <div ref={content => (this.content = content)} className={styles.main} style={{ width }}>
           {text}
         </div>
@@ -20,5 +26,18 @@ export default class extends React.Component {
     );
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this._updateWidth();
+  }
+
+  componentDidUpdate() {
+    this._updateWidth();
+  }
+
+  _updateWidth() {
+    const { content } = this;
+    if (content) {
+      this.setState({ scrollWidth: content.scrollWidth, clientWidth: content.clientWidth });
+    }
+  }
 }
